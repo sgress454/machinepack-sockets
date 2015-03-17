ccced348797eddd3935fc188244bbc993a14b116
Mike McNeil <mike@balderdash.co>
2015-03-16 22:52:37 -0700

    Update join.js

--- machines/join.js
+++ machines/join.js
@@ -1,5 +1,5 @@
 module.exports = {
-  friendlyName: 'Join',
+  friendlyName: 'Join room',
   description: 'Subscribes the specified sockets to a room.',
   extendedDescription: 'Subscribes the sockets to the room with the specified name.  Any messages subsequently broadcast to the room will be received by those sockets.',
   inputs: {
